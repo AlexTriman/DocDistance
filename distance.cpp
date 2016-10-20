3891b2823b039ea42095b569445eb6dcdf2f211c
Alex <olek.u7@gmail.com>
2016-10-20 07:00:09 +0300

    Program makes two vectors with occurancies of each word. You need: make a dotproduct of these vectors...

--- distance.cpp
+++ distance.cpp
@@ -8,48 +8,91 @@
 #include <cmath>
 #include <numeric>
 
-
-double DocDistance (std::vector<std::string> &v1, std::vector<std::string> &v2) {		         
-    std::cout << std::endl << std::inner_product( v1.begin(), v1.end(), v2.begin(), 0 ) << std::endl;
+/*
+double DocDistance (std::vector<std::string> & v1, std::vector<std::string> & v2) {		         
+    std::cout << std::endl << std::inner_product( v1.begin(), v1.end(), v2.begin(), " " ) << std::endl;
 	return 0;
+}
+*/
+
+void doc_Distance(std::vector<std::string> & v1, std::vector <std::string> & v2){
+	
+	
+	std::sort(v1.begin(), v1.end());
+	std::sort(v2.begin(), v2.end());
+	int k=0;
+	if (v1.size()>=v2.size()) k=v1.size();
+	else k=v2.size();
+	
+	std::vector<int>d1(0,0); // vector d1 with ocuurancy of each word for v1
+	std::vector<int>d2(0,0); // vector d2 with occurancy with each word for v2
+	int n=0;
+	for (int i=0; i<k;) {
+		n=i+1;
+		d1.push_back(1);  //indicate that the word is at least one time occured in vector
+		while (v1[i]==v1[n]) { //how many next words are the same		
+			d1.back()++; //the number of occurancies of the word for v1
+			n++;			
+		}	
+		
+		for (int j=0; j<v2.size(); j++){			
+			if (v2[j]==v1[i]){          //we have found the word that matches								
+				d2.push_back(1); //at least one time the word occures in v2
+				int c=j+1;   //next index for next word
+				while (v2[j]==v2[c]) { //count how many times the word occures					
+					d2.back()++;
+					
+					c++;				
+				}
+				break;			
+			}	
+			
+		}
+		i=n;  //skip words that we have cheked 
+	}	
+	
+	//earesing vectors d1 and d2 from 0 elemets	
+	
+	
+	//display two vectors
+	for (int i=0; i<d1.size(); i++){
+		std::cout << d1[i] << "   "<< d2[i]  <<std::endl;
+	}
+	std::cout <<"good....."<< std::endl;		
+	
 }
 
 int main () {
 	
-	std::fstream infile ("text.txt");
+	std::fstream infile1 ("text.txt");
 	
-	if (!infile)
+	
+	if (!infile1)
 		std::cout<<"Unable to open file\n";
 	
 	//Read words from file
 	std::string word;		
-	std::vector<std::string> Dist(0,"");	
-	while (infile>>word) {		
-		Dist.push_back(word);				
-	}
-		
-	//sorting alphabeticaly
-	//sort using defult < comparation
-	std::sort(Dist.begin(), Dist.end());
+	std::vector<std::string> v1(0,"");	
 	
-	//how to get that array to have only unreapeted words?	
-	for (int i=0; i<Dist.size(); i++) {
-		for (int k=i+1; k<Dist.size(); k++) {
-			if (Dist[i]==Dist[k]) Dist.at(k)=" ";			
-		}		
+	while (infile1>>word) {		
+		v1.push_back(word);				
 	}
-	for (int i=0; i<Dist.size(); i++) {
-		if (Dist[i]==" ") Dist.erase(Dist.begin()+i);
-	}
-	for (int i=0; i<Dist.size(); i++) {
-		if (Dist[i]==" ") Dist.erase(Dist.begin()+i);
-	}
+	infile1.close();
 	
-	DocDistance(Dist, Dist);
+	std::fstream infile2 ("text2.txt");
+	if (!infile2)
+		std::cout<<"Unable to open file\n";
+	std::vector<std::string> v2(0,"");
+	while (infile2>>word) {
+		v2.push_back(word);
+	}
+	infile2.close();
+	
+	doc_Distance(v1,v2);
 	
 	//show the vector	
-	for (int i=0; i<Dist.size(); i++){
-		std::cout << Dist[i] << std::endl;
+	for (int i=0; i<v1.size(); i++){
+		std::cout << v1[i] << "   "<< v2[i]  <<std::endl;
 	}
 	std::cout <<"good....."<< std::endl;
 	return 0;
